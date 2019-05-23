1c0e7ba7f54ff35bdba8219f736e3e52a20e212b
KBonnicksen <kelseybon@gmail.com>
2019-05-22 21:44:17 -0700

    Created constructors

--- Student.cs
+++ Student.cs
@@ -6,14 +6,68 @@ using System.Threading.Tasks;
 
 namespace StudentManagementSystem
 {
+    /// <summary>
+    /// Represents an individual Clover Park student
+    /// </summary>
     class Student
     {
+        //This is the default no-argws constructor
+        //The sompiler will create a no-args constructor
+        //if no other constructors are found
+        public Student() { }
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+        /// <summary>
+        /// The legal first name of the student
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The legal last name of the student
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The ID of the student. Auto-incremented value
+        /// </summary>
         public int StudentId { get; set; }
 
+        /// <summary>
+        /// The date the student was born. Time portion is ignored
+        /// </summary>
         public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The name of the program the student is registered in
+        /// ex. Computer Programming
+        /// </summary>
+        public string ProgramOfChoice { get; set; }
+        /// <summary>
+        /// A read-only property that returns the full legal name
+        /// (first + last)
+        /// </summary>
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        /// <summary>
+        /// Returns all student data split by a seperator
+        /// </summary>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public string GetDisplayText(string separator)
+        {
+            return $"{LastName},{FirstName}{separator}" +
+                $"{DateOfBirth.ToShortDateString()}{separator}";
+        }
     }
 }
