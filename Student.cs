b483331dce126e2893ade89c3138fb592886eb01
KBonnicksen <kelseybon@gmail.com>
2019-05-22 21:16:13 -0700

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int StudentId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
